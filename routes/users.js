36b5dc133dcd28d4f9519f2af7fd6ccd84cd5f6f
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-07-22 22:10:29 -0400

    created Post for new friend request

--- routes/users.js
+++ routes/users.js
@@ -105,7 +105,34 @@ router.post('/:id/post', async (req, res) => {
     return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });    
-////////////////////////////////////////////////////////// GET all Posts//////////////////////////////////////////
+////////////////////////////////////////////////////////// POST new friend Request //////////////////////////////////////////
+router.post('/:id/friendRequestIn', async (req, res) => {
+    try {
+        const { error } = (req.body);  // validateUser
+        if (error)
+        return res.status(400).send(error);
+
+        const user = await User.findById(req.params.id)
+
+    const friendRequestIn = new FriendRequestIn({
+
+    receiver: req.body.receiver,
+    status: req.body.status,
+     
+  
+    });
+
+    user.friendRequestIn.push(friendRequestIn)
+
+    await user.save();
+    return res.send(user);
+
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});    
+
+////////////////////////////////////////////////////////// GET all Posts for User//////////////////////////////////////////
 router.get('/:id/post', async (req, res) => {
     //TODO: refactor to get ALL users by videoId
     try {
@@ -118,6 +145,25 @@ router.get('/:id/post', async (req, res) => {
     return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+//////////////////////////////////////////////////////////////////// PUT  Likes (still nededs work)////////////////////////////////////////
+router.put('/:id/post/:id/likes', async (req, res) => {
+    
+    
+    try {
+        const user = await User.findById(req.params.id)
+        const post = await Post.findById(req.params.id) 
+
+    if (!post)
+    return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
+
+    post.likes++
+
+    await post.save();
+    return res.send(user.post);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+}); 
 //////////////////////////////////////////////////////////////////// DELETE Post (neeeds to be corrected)///////////////////////////////////////
 router.delete('/:userId/post/:postId', async (req, res) => {
     try {
@@ -135,24 +181,6 @@ router.delete('/:userId/post/:postId', async (req, res) => {
     return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 }); 
-/*
-//////////////////////////////////////////////////////////////////// PUT  Likes ////////////////////////////////////////
-router.put('/:id/likes', async (req, res) => {
-    try {
-
-        const post = await Comment.findById(req.params.id) 
-
-    if (!post)
-    return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
-
-    post.likes++
-
-    await post.save();
-    return res.send(post);
-    } catch (ex) {
-    return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-}); 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-*/
+
 module.exports = router;
