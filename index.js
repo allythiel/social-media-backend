9cb03c4240cebef4840b8262a10251eabfd39c8a
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2022-02-20 22:07:22 -0500

    changed port number

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors())
 app.use('/api/users', users);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
  console.log(`Server started on port: ${port}`);
 });
